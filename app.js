ae6f2eb15fdc86d4aa4ebcec1c71640ec9e93724
ZXY-jp <pei8925@gmail.com>
2022-01-20 00:41:21 +0800

    done!
    全數完成囉！

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-for (let i = 0; i < text.length; i++) {
-  console.log(`text number ${i} length is ${text[i].getTotalLength()}`);
-}
+// for (let i = 0; i < text.length; i++) {
+//   console.log(`text number ${i} length is ${text[i].getTotalLength()}`);
+// }
 
-// 把字體消除(透明)
-// seconde動畫結束後才會消失
-//讓後續畫面顯示
-const lastWord = document.querySelector("#second");
-const animation = document.querySelector("div.animation");
-lastWord.addEventListener("animationend", () => {
-  animation.style =
-    "transition: all 1s ease; opacity: 0; pointer-events: none;";
-});
+// // 把字體消除(透明)
+// // seconde動畫結束後才會消失
+// //讓後續畫面顯示
+// const lastWord = document.querySelector("#second");
+// const animation = document.querySelector("div.animation");
+// lastWord.addEventListener("animationend", () => {
+//   animation.style =
+//     "transition: all 1s ease; opacity: 0; pointer-events: none;";
+// });
 
-function setsubmit() {
-  if (myWorks.value == 0) window.location = "http://www.google.com";
-  if (myWorks.value == 1) window.location = "http://www.163.com";
-  if (myWorks.value == 2) window.location = "http://www.sina.com";
-}
+// function setsubmit() {
+//   if (myWorks.value == 0) window.location = "http://www.google.com";
+//   if (myWorks.value == 1) window.location = "http://www.163.com";
+//   if (myWorks.value == 2) window.location = "http://www.sina.com";
+// }
